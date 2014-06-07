229ad1ee50123607bb545f8276c68f7deef5892a
juhasch <python@elbonia.de>
2014-06-07 10:04:09 -0400

    Add multiplication and division to dB
    - add multiplication and division to dB unit (only if no physical
    dimension is given)
    - add dB() convenience function

--- PhysicalQuantities/dBUnit.py
+++ PhysicalQuantities/dBUnit.py
@@ -8,6 +8,13 @@ Example:
 
 """
 
+# TODO:
+# Conversion from a PhysicalQuantity to correct dB<x> value
+
+def dB(x):
+    """ Convert value to 10*log10 """
+    return dBUnit(10*np.log10(x),'dB',islog=True)
+
 import numpy as np
 import copy
 
@@ -15,7 +22,7 @@ import re
 import PhysicalQuantities as pq
 
 # list of tuples: (base unit, correction factor to base unit, conversion factor to linear)
-dB_units = {'dB':  ('', 0, 1),
+dB_units = {'dB':  ('', 0, 10),
             'dBm':  ('W', -30, 10),  # Power in Watt
             'dBW':  ('W', 0, 10),
             'dBnV': ('V', -90, 20),  # Voltage
@@ -40,7 +47,7 @@ class dBUnit(object):
     def __init__(self, value, unit, **kwargs):
         # initialize and convert to logarithm if islog=False
         islog = False
-        self.z0 = 50
+        self.z0 = 50.
         try:
             ip = get_ipython()
             self.ptformatter = ip.display_formatter.formatters['text/plain']
@@ -151,7 +158,7 @@ class dBUnit(object):
             value = self.value - other.value
             return self.__class__(value, self.unit, islog=True)
         elif dB_units[self.unit][0] is dB_units[other.unit][0]:
-            # same unit substraction
+            # same unit subtraction
             val1 = float(self)
             val2 = float(other)
             return self.__class__(val1-val2, self.unit, islog=False)
@@ -159,6 +166,22 @@ class dBUnit(object):
             raise UnitError('Cannot add unequal units %s and %s' % (self.unit, other.unit))
 
     __rsub__ = __sub__
+    
+    def __mul__(self, other):
+        if self.unit is 'dB' and not hasattr(other,'unit'):
+            # dB without physical dimension can be multiplied with a factor
+            print self,self.value,other,value
+            return self.__class__(value, self.unit, islog=True)
+
+    __rmul__ = __mul__
+
+    def __div__(self, other):
+        if self.unit is 'dB' and not hasattr(other,'unit'):
+            # dB without physical dimension can be divided by a factor
+            value = self.value / other
+            return self.__class__(value, self.unit, islog=True)
+
+    __rdiv__ = __div__
 
     def __float__(self):
         # return linear value in base unit
