09d3b7051f0642bf3b5879ad13da4f8a2fd3144c
Jürgen Hasch <juergen.hasch@elbonia.de>
2017-03-05 14:56:13 +0100

    Configure readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'PhysicalQuantities'
-copyright = '2016, Juergen Hasch'
+copyright = '2017, Juergen Hasch'
 author = 'Jürgen Hasch'
 
 # The version info for the project you're documenting, acts as replacement for
