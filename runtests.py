69b712be5fc78a051e410be181c9421b07ee00e5
Juergen Hasch <juergen.hasch@mmwaves.de>
2022-07-11 10:14:47 +0200

    Fix requirements

--- runtests.py
+++ runtests.py
@@ -1,6 +1,6 @@
 import subprocess
 
-cmd = "py.test --cov=PhysicalQuantities --cov-report html tests/"
+cmd = "pytest --cov --cov-report html tests/"
 
 p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 for line in p.stdout.readlines():
