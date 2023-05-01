03feaa6250af68d81b7c748e3582e6a1cfb36b13
juhasch <juergen.hasch@mmwaves.de>
2023-05-01 14:51:44 +0200

    Generic path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,7 @@ import PhysicalQuantities
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(1, '/Users/juhasch/git/PhysicalQuantities/')
-                #os.path.abspath('..'))
-
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
@@ -44,7 +42,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-source_suffix = [ '.rst']
+source_suffix = ['.rst']
 
 # The encoding of source files.
 source_encoding = 'utf-8'
